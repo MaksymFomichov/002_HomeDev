5f5047d372e36fd486a85eee8d0695637047b7c5
MaksymFomichov <m.fomichov@gmail.com>
2017-11-30 16:58:32 +0200

    убрал лишние файлы

--- src/main/resources/3.sql
+++
@@ -1,8 +0,0 @@
-/*Вычислить общую ЗП всех Java разработчиков. */
-
-SELECT sum(developers.salary)
-FROM developers_skills
-  JOIN developers ON developers.id = developers_skills.developers_id
-  JOIN skills ON skills.id = developers_skills.skills_id
-WHERE skills.skill = "java"
-
